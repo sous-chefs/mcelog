77cf7bcc976614236c6f36fe931deec13c4ffa88
Tim Smith <tim@cozy.co>
2015-08-12 23:20:12 -0700

    Add chef 12 metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
-name             'mcelog'
-maintainer       'Tim Smith - Limelight Networks, Inc.'
+name 'mcelog'
+maintainer 'Tim Smith - Limelight Networks, Inc.'
 maintainer_email 'tsmith@llnw.com'
-license          'Apache 2.0'
-description      'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+license 'Apache 2.0'
+description 'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
+version '0.1.5'
 
 %w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
   supports os
 end
+source_url 'https://github.com/tas50/mcelog' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/mcelog/issues'  if respond_to?(:issues_url)
