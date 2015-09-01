7be278019a96cff5bc51052ccc1013c2df73eda4
Tim Smith <tsmith84@gmail.com>
2015-08-31 23:07:59 -0700

    Update maintainers

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'mcelog'
-maintainer 'Tim Smith - Limelight Networks, Inc.'
-maintainer_email 'tsmith@llnw.com'
+maintainer 'Tim Smith'
+maintainer_email 'tsmith84@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
 version '0.1.5'
@@ -8,5 +8,6 @@ version '0.1.5'
 %w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
   supports os
 end
+
 source_url 'https://github.com/tas50/mcelog' if respond_to?(:source_url)
 issues_url 'https://github.com/tas50/mcelog/issues' if respond_to?(:issues_url)
