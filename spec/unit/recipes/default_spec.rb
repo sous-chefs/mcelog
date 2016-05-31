dc3229f57f8a19abd5866bde115e698e01321dbd
Tim Smith <tsmith@chef.io>
2016-05-31 12:34:36 -0700

    Expand specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,7 +6,50 @@ describe 'default recipe on Ubuntu 16.04' do
       runner.converge('mcelog::default')
   end
 
-  it 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
+  it 'installs mcelog' do
+   expect(chef_run).to install_package('mcelog')
+  end
+
+  it 'creates /etc/mcelog' do
+   expect(chef_run).to create_directory('/etc/mcelog')
+  end
+
+  it 'starts mcelog service' do
+   expect(chef_run).to start_service('mcelog')
+  end
+
+  it 'enables mcelog service' do
+   expect(chef_run).to enable_service('mcelog')
+  end
+end
+
+
+describe 'default recipe on CentOS 6' do
+      cached(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7')
+      runner.converge('mcelog::default')
+  end
+
+  it 'starts mcelog service' do
+   expect(chef_run).to start_service('mcelogd')
+  end
+
+  it 'enables mcelog service' do
+   expect(chef_run).to enable_service('mcelogd')
+  end
+end
+
+describe 'default recipe on CentOS 7' do
+      cached(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
+      runner.converge('mcelog::default')
+  end
+
+  it 'starts mcelog service' do
+   expect(chef_run).to start_service('mcelog')
+  end
+
+  it 'enables mcelog service' do
+   expect(chef_run).to enable_service('mcelog')
   end
 end
