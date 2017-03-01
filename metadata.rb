94aa8ebba021fce04580e637387a86eabff93be3
Tim Smith <tsmith@chef.io>
2017-03-01 09:29:27 -0800

    Require Chef 12.1+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,6 @@ version '1.0.0'
   supports os
 end
 
-source_url 'https://github.com/tas50/chef-mcelog' if respond_to?(:source_url)
-issues_url 'https://github.com/tas50/chef-mcelog/issues' if respond_to?(:issues_url)
-
-chef_version '>= 11.0' if respond_to?(:chef_version)
+source_url 'https://github.com/chef-cookbooks/mcelog'
+issues_url 'https://github.com/chef-cookbooks/mcelog/issues'
+chef_version '>= 12.1' if respond_to?(:chef_version)
