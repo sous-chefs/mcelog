ee9b81eaae01b02730d20e62934eb5f8d5e504e4
Tim Smith <tsmith@chef.io>
2017-03-21 16:02:28 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'mcelog'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
