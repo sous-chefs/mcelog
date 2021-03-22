0578856f1a2742bffc0f964797454afbbe2a82f9
Ethan Hampton <hamptone@osuosl.org>
2021-03-22 12:13:30 -0700

    Update metadata
    Signed-off-by: Ethan Hampton <hamptone@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,15 @@
 name 'mcelog'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license 'Apache-2.0'
-description 'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
-version '2.0.1'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs/Configures mcelog to catch and decode hardware errors from the Linux kernel'
+version           '2.0.1'
+source_url        'https://github.com/sous-chefs/mcelog'
+issues_url        'https://github.com/sous-chefs/mcelog/issues'
+chef_version      '>= 12.15'
 
-%w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/mcelog'
-issues_url 'https://github.com/chef-cookbooks/mcelog/issues'
-chef_version '>= 12.15'
+supports 'debian'
+supports 'centos'
+supports 'redhat'
+supports 'oracle'
+supports 'scientific'
