7174f2d8ed42f61e3e6b78f3bcdc6baed8aead4e
Dan Webb <dan.webb@damacus.io>
2026-04-28 23:12:49 +0100

    test: align unit tests with supported platform matrix

--- spec/unit/resources/mcelog_spec.rb
+++ spec/unit/resources/mcelog_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 describe 'test::default' do
   step_into :mcelog
 
-  context 'when on Ubuntu' do
-    platform 'ubuntu'
+  context 'when on AlmaLinux 8' do
+    platform 'almalinux', '8'
 
     before do
       stub_command('mcelog --is-cpu-supported').and_return(true)
@@ -17,7 +17,7 @@ describe 'test::default' do
     end
 
     it 'creates the trigger directory' do
-      is_expected.to create_directory('/etc/mcelog')
+      is_expected.to create_directory('/etc/mcelog/triggers')
     end
 
     it 'renders the mcelog.conf template' do
@@ -25,23 +25,6 @@ describe 'test::default' do
     end
 
     it 'enables and starts the service' do
-      is_expected.to enable_service('mcelog')
-      is_expected.to start_service('mcelog')
-    end
-  end
-
-  context 'when on CentOS 7' do
-    platform 'centos', '7'
-
-    before do
-      stub_command('mcelog --is-cpu-supported').and_return(true)
-    end
-
-    it 'creates the trigger directory' do
-      is_expected.to create_directory('/etc/mcelog/triggers')
-    end
-
-    it 'enables and starts the mcelog service' do
       is_expected.to enable_service('mcelog')
       is_expected.to start_service('mcelog')
     end
